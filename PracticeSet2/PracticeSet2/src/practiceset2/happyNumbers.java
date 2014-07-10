181c5907e2aab39ad50b70cdd653f3e6529b9f80
lud200 <dludaya09@outlook.com>
2014-07-10 16:26:53 -0400

    test

--- PracticeSet2/PracticeSet2/src/practiceset2/happyNumbers.java
+++ PracticeSet2/PracticeSet2/src/practiceset2/happyNumbers.java
@@ -1,5 +1,16 @@
 package practiceset2;
 
 public class happyNumbers {
+	public static void main(String[] args){
+		int n=7;
+		int temp1=0, temp2=0;
+		temp1=n;
+		while(n!=1){
+			temp1=n%10;
+			temp2=temp1*temp1+temp2;
+			n=temp2;
+		}
+		System.out.println(n);
+	}
 
 }
