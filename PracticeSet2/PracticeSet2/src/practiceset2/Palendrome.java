01cbd5e2079dbf439840429a442e30b9968987d6
lud200 <dludaya09@outlook.com>
2014-08-16 22:39:21 -0400

    update

---
+++ PracticeSet2/PracticeSet2/src/practiceset2/Palendrome.java
@@ -0,0 +1,21 @@
+package practiceset2;
+import java.io.*;
+import java.util.*;
+
+public class Palendrome {
+	
+	public static void main(String[] args){
+		
+		String s="Ilikeracecarsthatgofast";
+		int i=49;
+		if(i%35==0){
+			System.out.println("FooBar");
+		}else if(i%5==0){
+			System.out.println("Foo");
+		}
+		else if(i%7==0){
+			System.out.println("Bar");
+		}
+		
+	}
+}
--- PracticeSet2/PracticeSet2/src/practiceset2/happyNumbers.java
+++ PracticeSet2/PracticeSet2/src/practiceset2/happyNumbers.java
@@ -3,14 +3,12 @@ package practiceset2;
 public class happyNumbers {
 	public static void main(String[] args){
 		int n=7;
-		int temp1=0, temp2=0;
-		temp1=n;
-		while(n!=1){
-			temp1=n%10;
-			temp2=temp1*temp1+temp2;
-			n=temp2;
-		}
-		System.out.println(n);
+		int i=0, j=0;
+		
+			n=n%10;
+			i=n*n;
+		
+		System.out.println(i);
 	}
 
 }
